0d931087f52823547ee0ac5ccf82a19779564524
Sergey S <PRESENT@chmp.local>
2019-08-26 09:37:58 +0400

    Task 5

---
+++ bak/1/myApp6.rb
@@ -0,0 +1,7 @@
+print "Enter your name: "
+myName = gets.chomp
+
+print "Enter your age: "
+age = gets.chomp
+
+puts "Hello " + myName + " your age is " + age + " !!!"
--- myApp1.rb
+++ myApp1.rb
@@ -1,1 +1,3 @@
-puts "Hello"
+4.upto(20) { |x| puts x }
+-4.upto(5) { |x| puts x }
+10.downto(5) { |x| puts x }
